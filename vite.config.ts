8c3a223e0744a9bcd7c00d18282ce539943835c2
Decante <gustavocamposantunes@gmail.com>
2025-05-21 23:25:11 -0300

    chore: include svgr as plugin in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), tsConfigPaths()],
+  plugins: [tailwindcss(), react(), tsConfigPaths(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
