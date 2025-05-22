f1b35e5d2a98df02b2f2ea4fa63ce5ed4efe27fa
Decante <gustavocamposantunes@gmail.com>
2025-05-22 03:47:49 -0300

    chore: config QueryClientProvider

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Router } from './navigation/router'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </StrictMode>,
 )
