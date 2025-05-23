8fa7a72b2eeb5d4724c55b1d9634dcdeba37e499
Decante <gustavocamposantunes@gmail.com>
2025-05-22 22:28:43 -0300

    style: add size and font-weight to FormHeaderAction title

--- src/presentation/components/molecules/FormHeaderAction.tsx
+++ src/presentation/components/molecules/FormHeaderAction.tsx
@@ -12,7 +12,7 @@ export const FormHeaderAction: React.FC<IFormHeaderAction> = ({
   const navigate = useNavigate()
   return (
     <section className="flex justify-between w-full">
-      <h2>{title}</h2>
+      <h2 className="text-2xl font-semibold">{title}</h2>
       <div className="flex gap-2">
         <Button className="orange-btn action-btn" type="button" onClick={() => navigate("/posts")}>
           Cancelar
