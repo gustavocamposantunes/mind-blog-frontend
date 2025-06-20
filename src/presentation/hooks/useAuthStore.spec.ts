e220fd2d402bea8ede705f9084e7d28196d8a62b
Decante <gustavocamposantunes@gmail.com>
2025-06-20 15:30:03 -0300

    test: ensure useAuthStore clear current user

--- src/presentation/hooks/useAuthStore.spec.ts
+++ src/presentation/hooks/useAuthStore.spec.ts
@@ -33,4 +33,18 @@ describe("useAuthStore", () => {
     expect(currentUser.user.name).toBe(account.user.name);
     expect(currentUser.user.email).toBe(account.user.email);
   });
+
+  it("should clear current user", () => {
+    const account: AuthenticateUserModel = mockAuthenticateUserModel();
+
+    useAuthStore.getState().setCurrentUser(account);
+    useAuthStore.getState().clearCurrentUser();
+
+    const currentUser = useAuthStore.getState().getCurrentUser();
+
+    expect(currentUser.accessToken).toBe("");
+    expect(currentUser.user.id).toBe(0);
+    expect(currentUser.user.name).toBe("");
+    expect(currentUser.user.email).toBe("");
+  });
 });
