fc053615d509150fc30082085cf34becc01c43ca
Decante <gustavocamposantunes@gmail.com>
2025-06-20 19:50:34 -0300

    test: ensure LoginPage redirects to HomePage on authentication success

--- src/presentation/pages/LoginPage.spec.tsx
+++ src/presentation/pages/LoginPage.spec.tsx
@@ -1,37 +1,68 @@
-import { describe, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 import { LoginPage } from "./LoginPage";
 import { UnexpectedError } from "@/domain/errors";
 import { faker } from "@faker-js/faker";
-import { fireEvent, render, screen } from "../test/test-utils";
+import { fireEvent, render, screen, waitFor } from "../test/test-utils";
 import { AuthenticateUserSpy } from "../test/mock-authenticate-user";
 
-vi.mock('react-router-dom', () => ({
-  useNavigate: vi.fn()
+const mockNavigate = vi.fn()
+
+vi.mock("react-router-dom", async () => ({
+  ...await vi.importActual("react-router-dom"),
+  useNavigate: () => mockNavigate
 }))
 
+type SutTypes = {
+  authenticateUserSpy: AuthenticateUserSpy
+}
 
+const makeSut = (): SutTypes => {
+  const authenticateUserSpy = new AuthenticateUserSpy();
+
+  render(
+    <LoginPage authenticateUser={authenticateUserSpy} />
+  );
+
+  return {
+    authenticateUserSpy
+  }
+}
 
 describe("LoginPage", () => {
-  it("should render a tooltip.error with a message if the authentication fails", async () => {
-    const authenticateUserSpy = new AuthenticateUserSpy()
-
-    const error = new UnexpectedError();
-    vi.spyOn(authenticateUserSpy, 'auth').mockRejectedValueOnce(error);
-
-    render(
-      <LoginPage authenticateUser={authenticateUserSpy}/>
-    );
-
+  const setupSubmit = () => {
     const emailInput = screen.getByLabelText(/email/i);
     const passwordInput = screen.getByLabelText(/senha/i);
+
     const submitButton = screen.getByRole('button', { name: /entrar/i });
 
     fireEvent.change(emailInput, { target: { value: faker.internet.email() } });
     fireEvent.change(passwordInput, { target: { value: faker.internet.password() } });
 
     fireEvent.click(submitButton);
+  }
+
+  it("should render a tooltip.error with a message if the authentication fails", async () => {
+    const authenticateUserSpy = new AuthenticateUserSpy();
+
+    const error = new UnexpectedError();
+    vi.spyOn(authenticateUserSpy, 'auth').mockRejectedValueOnce(error);
+
+    render(
+      <LoginPage authenticateUser={authenticateUserSpy} />
+    );
+
+    setupSubmit();
 
     await screen.findByText(error.message);
+  });
+
+  it("should redirects to HomePage on authentication success", async () => {
+    makeSut();
+    setupSubmit();
+
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith("/");
+    });
   });
 });
---
+++ src/presentation/test/mock-authenticate-user.ts
@@ -0,0 +1,16 @@
+import type { AuthenticateUserUseCase, AuthParams } from "@/domain/usecases";
+import type { HttpRemoteResponse } from "@/data/protocols";
+import type { AuthenticateUserModel } from "@/domain/models";
+import { mockAuthenticateUserModel } from "@/domain/test";
+
+export class AuthenticateUserSpy implements AuthenticateUserUseCase {
+  authenticationParams: AuthParams | undefined;
+  async auth(authenticationParams: AuthParams): Promise<HttpRemoteResponse<AuthenticateUserModel>> {
+    this.authenticationParams = authenticationParams
+
+    return Promise.resolve({
+      statusCode: 200,
+      data: mockAuthenticateUserModel()
+    })
+  }
+}
