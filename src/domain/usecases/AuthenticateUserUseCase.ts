b2da5442226445d68b13dae1e1f11f986762a8fe
Decante <gustavocamposantunes@gmail.com>
2025-06-20 11:29:06 -0300

    refactor: change AuthenticateUserUseCase to use HttpRemoteResponse return

--- src/domain/usecases/AuthenticateUserUseCase.ts
+++ src/domain/usecases/AuthenticateUserUseCase.ts
@@ -1,11 +1,8 @@
+import type { HttpRemoteResponse } from "@/data/protocols"
 import type { AuthenticateUserModel } from "../models"
 
 export type AuthParams ={ email: string, password: string }
 
 export interface AuthenticateUserUseCase {
-  auth(authenticationParams: AuthParams): Promise<{
-    statusCode: number;
-    data?: AuthenticateUserModel;
-    error?: string;
-  }>
+  auth(authenticationParams: AuthParams): Promise<HttpRemoteResponse<AuthenticateUserModel>>
 }
