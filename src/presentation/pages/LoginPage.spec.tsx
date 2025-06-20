48ffcbc2b8cdcd3d4c793a158e70e87e7993b8d7
Decante <gustavocamposantunes@gmail.com>
2025-06-20 19:16:01 -0300

    refactor: move AuthenticateUserSpy to a separate file

--- src/presentation/pages/LoginPage.spec.tsx
+++ src/presentation/pages/LoginPage.spec.tsx
@@ -1,32 +1,19 @@
 import { describe, it, vi } from "vitest";
 
 import { LoginPage } from "./LoginPage";
-import type { AuthenticateUserUseCase, AuthParams } from "@/domain/usecases";
-import type { HttpRemoteResponse } from "@/data/protocols";
-import type { AuthenticateUserModel } from "@/domain/models";
-import { mockAuthenticateUserModel } from "@/domain/test";
 import { UnexpectedError } from "@/domain/errors";
 import { faker } from "@faker-js/faker";
 import { fireEvent, render, screen } from "../test/test-utils";
+import { AuthenticateUserSpy } from "../test/mock-authenticate-user";
 
 vi.mock('react-router-dom', () => ({
   useNavigate: vi.fn()
 }))
 
-export class AuthenticateUserSpy implements AuthenticateUserUseCase {
-  authenticationParams: AuthParams | undefined;
-  async auth(authenticationParams: AuthParams): Promise<HttpRemoteResponse<AuthenticateUserModel>> {
-    this.authenticationParams = authenticationParams
 
-    return Promise.resolve({
-      statusCode: 200,
-      data: mockAuthenticateUserModel()
-    })
-  }
-}
 
 describe("LoginPage", () => {
-  it("Should render a tooltip.error with a message if the authentication fails", async () => {
+  it("should render a tooltip.error with a message if the authentication fails", async () => {
     const authenticateUserSpy = new AuthenticateUserSpy()
 
     const error = new UnexpectedError();
