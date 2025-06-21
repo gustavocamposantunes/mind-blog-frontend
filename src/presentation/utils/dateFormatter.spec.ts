05e6f61584e0681cad867a954202761528355be8
Decante <gustavocamposantunes@gmail.com>
2025-06-21 16:17:36 -0300

    test: ensure formatDateToShortMonth format date to short month

---
+++ src/presentation/utils/dateFormatter.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+import { formatDateToShortMonth } from './dateFormatter';
+
+describe("dateFormatter", () => {
+  describe("formatDateToShortMoth", () => {
+    it("should format date to short month", () => {
+      const date = "2023-10-01T00:00:00Z";
+      const formattedDate = formatDateToShortMonth(date);
+      expect(formattedDate).toBe("Out 1, 2023");
+    });
+
+    it("should capitalize the first letter of the formatted date", () => {
+      const date = "2023-10-01T00:00:00Z";
+      const formattedDate = formatDateToShortMonth(date);
+      expect(formattedDate.charAt(0)).toBe("O");
+    });
+  });
+});
