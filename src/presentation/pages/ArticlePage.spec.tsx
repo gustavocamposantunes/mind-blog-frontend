f90ad5d64abd3183b1c5acbd66b26caf9f211178
Decante <gustavocamposantunes@gmail.com>
2025-06-21 03:05:56 -0300

    test: ensure error-wrapper show a error message on fail

--- src/presentation/pages/ArticlePage.spec.tsx
+++ src/presentation/pages/ArticlePage.spec.tsx
@@ -1,7 +1,8 @@
-import { describe, expect, it, vi } from "vitest";
-import { render, screen } from "../test/test-utils";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { cleanup, render, screen } from "../test/test-utils";
 import { ArticlePage } from "./ArticlePage";
 import { GetArticleByIdSpy } from "../test";
+import { UnexpectedError } from "@/domain/errors";
 
 vi.mock("react-router-dom", async () => ({
   ...await vi.importActual("react-router-dom"),
@@ -25,11 +26,26 @@ const makeSut = (): SutTypes => {
 }
 
 describe("ArticlePage", () => {
+  beforeEach(cleanup);
   it("should render a skeleton group while the content is loading", async () => {
     makeSut();
 
     const skeletonGroup = await screen.findByTestId("skeleton-group")
 
     expect(skeletonGroup).toBeTruthy();
+  });
+
+  it("should render an error message if throws", async () => {
+    const getArticleByIdSpy = new GetArticleByIdSpy();
+
+    const error = new UnexpectedError()
+    vi.spyOn(getArticleByIdSpy, "getById").mockRejectedValueOnce(error)
+
+    render(
+      <ArticlePage getArticletById={getArticleByIdSpy} />
+    );
+
+    const errorWrapper = await screen.findByTestId("error-wrapper");
+    expect(errorWrapper.textContent).toBe("Erro inesperado");
   });
 });
