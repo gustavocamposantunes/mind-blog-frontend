d8c23dd6478268e15cd1e24eca2440af711ae8c8
Decante <gustavocamposantunes@gmail.com>
2025-06-21 14:34:19 -0300

    test: ensure RegisterUserPage render an error assistive text when password missmatch

--- src/presentation/pages/RegisterUserPage.spec.tsx
+++ src/presentation/pages/RegisterUserPage.spec.tsx
@@ -5,17 +5,33 @@ import { RegisterUserSpy } from "../test";
 import { UnexpectedError } from "@/domain/errors";
 import { faker } from "@faker-js/faker";
 
-const mockNavigate = vi.fn()
+const mockNavigate = vi.fn();
 
 vi.mock("react-router-dom", async () => ({
   ...await vi.importActual("react-router-dom"),
   useNavigate: () => mockNavigate
-}))
+}));
+
+type SutTypes = {
+  registerUserSpy: RegisterUserSpy
+}
+
+const makeSut = (): SutTypes => {
+  const registerUserSpy = new RegisterUserSpy();
+
+  render(
+    <RegisterUserPage registerUser={registerUserSpy} />
+  );
+
+  return {
+    registerUserSpy
+  }
+}
 
 describe("RegisterUserPage", () => {
   beforeEach(cleanup)
 
-  const setupSubmit = () => {
+  const setupSubmit = (missMatchPassowrd?: string) => {
     const nameInput = screen.getByLabelText(/nome/i);
     const emailInput = screen.getByLabelText(/email/i);
     const password = screen.getByPlaceholderText(/digite sua senha/i);
@@ -24,7 +40,7 @@ describe("RegisterUserPage", () => {
     const fakePassowrd = faker.internet.password()
     fireEvent.change(nameInput, { target: { value: faker.person.firstName() } });
     fireEvent.change(emailInput, { target: { value: faker.internet.email() } });
-    fireEvent.change(password, { target: { value: fakePassowrd } });
+    fireEvent.change(password, { target: { value: missMatchPassowrd ?? fakePassowrd } });
     fireEvent.change(passwordConfirmation, { target: { value: fakePassowrd } });
 
     const submitButton = screen.getByRole('button', {
@@ -33,6 +49,7 @@ describe("RegisterUserPage", () => {
 
     fireEvent.click(submitButton);
   }
+
   it("should render a toast.error when submit fails", async () => {
     const registerUserSpy = new RegisterUserSpy();
 
@@ -48,5 +65,17 @@ describe("RegisterUserPage", () => {
     const errorToastMessage = await screen.findByText(error.message);
 
     expect(errorToastMessage).toBeTruthy();
+  });
+
+  it("should render an error assistive text when password missmatch", async () => {
+    makeSut();
+
+    const wrongPassword = faker.internet.password();
+
+    setupSubmit(wrongPassword);
+
+    const errorAssistiveText = await screen.findByText(/as senhas não coincidem/i);
+
+    expect(errorAssistiveText).toBeTruthy();
   });
 });
