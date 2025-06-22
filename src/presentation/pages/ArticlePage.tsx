d63e568138da3d1c3f389f4d76ea799adbdfcf9e
Decante <gustavocamposantunes@gmail.com>
2025-06-22 18:22:55 -0300

    refactor: remove default image from ArticlePage

--- src/presentation/pages/ArticlePage.tsx
+++ src/presentation/pages/ArticlePage.tsx
@@ -43,7 +43,7 @@ export const ArticlePage: React.FC<ArticlePageProps> = ({
                 <Heart />
               </span>
             </div>
-            <img className="mt-5 w-full" src={data?.data?.image ?? "https://miro.medium.com/v2/resize:fit:1358/1*moJeTvW97yShLB7URRj5Kg.png"} alt={data?.data?.title} />
+            <img className="mt-5 w-full" src={data?.data?.image} alt={data?.data?.title} />
             <p className="p-4">
               {data?.data?.content}
             </p>
