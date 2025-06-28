992ec29877cbf51ade8d5ed87dca358baad1886a
Decante <gustavocamposantunes@gmail.com>
2025-06-28 17:24:17 -0300

    fix: adjust domain models path

--- src/domain/usecases/article/ListArticles.usecase.ts
+++ src/domain/usecases/article/ListArticles.usecase.ts
@@ -1,5 +1,5 @@
-import type { ArticleListModel } from '../models'
 import type { HttpRemoteResponse } from '@/data/protocols'
+import type { ArticleListModel } from '@/domain/models'
 
 export interface ListArticlesUseCase {
   listAll(): Promise<HttpRemoteResponse<ArticleListModel>>
