e5926e4581ebff6b29eaa3375e42babddc0d0d1d
Decante <gustavocamposantunes@gmail.com>
2025-06-29 06:52:33 -0300

    feat: add rules for a11y in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ import jsxA11y from 'eslint-plugin-jsx-a11y'
 export default tseslint.config(
   { ignores: ['dist', 'coverage'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig, jsxA11y],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -21,10 +21,12 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       'import': pluginImport,
-      'prettier': prettierPlugin
+      'prettier': prettierPlugin,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
