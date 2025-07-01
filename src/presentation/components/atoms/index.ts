96db8553884f070105ae61580ccdfa50562c7188
Decante <gustavocamposantunes@gmail.com>
2025-07-01 15:32:41 -0300

    chore: update index for components

--- src/presentation/components/atoms/index.ts
+++ src/presentation/components/atoms/index.ts
@@ -1,3 +1,4 @@
+export * from './AuthForm'
 export * from './CustomSkeleton'
 export * from './FavouriteAvatarPost'
 export * from './MenuItem'
--- src/presentation/components/templates/index.ts
+++ src/presentation/components/templates/index.ts
@@ -1,2 +1,3 @@
 export * from './AuthTemplate'
+export * from './LoginTemplate'
 export * from './PageTemplate'
