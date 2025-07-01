8e3b8ea04d1dd6fe436741fead2d1ac36d74c1c3
Decante <gustavocamposantunes@gmail.com>
2025-07-01 12:06:45 -0300

    fix: add id to label CustomInput

--- src/presentation/components/molecules/CustomInput.tsx
+++ src/presentation/components/molecules/CustomInput.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { Input } from '../ui/input'
 import { Label } from '../ui/label'
 
@@ -11,7 +13,7 @@ export const CustomInput: React.FC<ICustomInput> = ({
   ...props
 }) => (
   <div className={`grid w-full items-center gap-1.5 ${className}`}>
-    <Label htmlFor="password" className="text-stone-950 text-lg md:text-base!">
+    <Label htmlFor={props.id} className="text-stone-950 text-lg md:text-base!">
       {label}
     </Label>
     <Input className="text-xl md:text-base!" {...props} />
