6b080e3523e7e7690bf1033e7481dfdeb605fe75
Decante <gustavocamposantunes@gmail.com>
2025-07-01 15:32:14 -0300

    feat: config addon-viewport in main storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
+    '@storybook/addon-viewport',
   ],
   framework: {
     name: '@storybook/react-vite',
