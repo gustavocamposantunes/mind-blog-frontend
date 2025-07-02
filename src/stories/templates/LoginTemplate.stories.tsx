3b371e4f61b3fb9c0f0860122d0f6d1932613673
Decante <gustavocamposantunes@gmail.com>
2025-07-01 23:56:02 -0300

    docs: create LoginTemplate storybook

---
+++ src/stories/templates/LoginTemplate.stories.tsx
@@ -0,0 +1,60 @@
+import { LogIn } from 'lucide-react'
+import { Link, MemoryRouter, Route, Routes } from 'react-router-dom'
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { AuthForm } from '@/presentation/components/atoms'
+import { TextField } from '@/presentation/components/molecules'
+import { LoginTemplate } from '@/presentation/components/templates'
+import { Button } from '@/presentation/components/ui/button'
+
+const meta = {
+  title: 'Templates/LoginTemplate',
+  component: () => (
+    <LoginTemplate>
+      <AuthForm onSubmit={() => {}}>
+        <TextField
+          className="mt-8"
+          label="Email"
+          type="email"
+          id="email"
+          placeholder="Digite seu email"
+        />
+        <TextField
+          className="mt-8"
+          label="Senha"
+          type="password"
+          id="password"
+          placeholder="Digite sua senha"
+        />
+
+        <Link to="/forgot-password" className="text-end mt-4">
+          Esqueceu a senha?
+        </Link>
+
+        <Button className="mt-4 w-full py-6" type="submit">
+          Entrar
+          <LogIn />
+        </Button>
+      </AuthForm>
+    </LoginTemplate>
+  ),
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Story />} />
+        </Routes>
+      </MemoryRouter>
+    ),
+  ],
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
