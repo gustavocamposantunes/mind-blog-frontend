58fd2fbf988ea53885d18c4bd0ddd027e96d546a
Decante <gustavocamposantunes@gmail.com>
2025-07-02 23:38:43 -0300

    feat: create makeApiUrlWithParams

--- src/main/factories/http/make-api-url.ts
+++ src/main/factories/http/make-api-url.ts
@@ -1,2 +1,22 @@
-export const makeApiUrl = (path: string) =>
-  `${import.meta.env.VITE_MOCKOON_URL}${path}`
+type ApiType = 'mockoon' | 'news'
+
+const baseUrls: Record<ApiType, string> = {
+  mockoon: import.meta.env.VITE_MOCKOON_URL,
+  news: import.meta.env.VITE_NEWS_API,
+}
+
+export const makeApiUrl = (path: string, type: ApiType = 'mockoon'): string => {
+  return `${baseUrls[type]}${path}`
+}
+
+export const makeApiUrlWithParams = (
+  basePath: string,
+  params: Record<string, string>,
+  type: ApiType = 'mockoon',
+): string => {
+  const url = new URL(makeApiUrl(basePath, type))
+  Object.entries(params).forEach(([key, value]) => {
+    url.searchParams.set(key, value)
+  })
+  return url.toString()
+}
