01a616d50cff723338a121dbbb4ef8eb2a98264e
Decante <gustavocamposantunes@gmail.com>
2025-07-03 00:21:16 -0300

    docs(storybook): create News storybook

---
+++ src/stories/organisms/News.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { mockNews } from '@/domain/test'
+import { News } from '@/presentation/components/organism'
+
+const meta = {
+  title: 'Organisms/News',
+  component: News,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof News>
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    news: mockNews().articles,
+  },
+}
