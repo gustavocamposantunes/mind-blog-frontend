ba105aa21643553a35c48a532ffaf8df6553616d
decante <gustavocamposantunes@gmail.com>
2025-07-10 14:51:35 -0300

    feat: ensure EditArticlePage show an toast.error if an article is not Found

---
+++ src/presentation/pages/EditArticlePage.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+import { useGetArticleById } from '../hooks'
+
+import type { GetArticleByIdUseCase } from '@/domain/usecases'
+
+type EditArticlePageProps = {
+  getArticletById: GetArticleByIdUseCase
+}
+
+export const EditArticlePage: React.FC<EditArticlePageProps> = ({
+  getArticletById,
+}) => {
+  const { id } = useParams<{ id: string }>()
+  const { error } = useGetArticleById(getArticletById, String(id))
+
+  useEffect(() => {
+    toast.error('Erro ao buscar artigo')
+  }, [error])
+
+  return <></>
+}
