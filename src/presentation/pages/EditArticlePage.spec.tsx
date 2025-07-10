7198be6096675f546a63db3f1b3ed89ff7f3e514
decante <gustavocamposantunes@gmail.com>
2025-07-10 16:35:54 -0300

    fix: change redundant role to form

--- src/presentation/pages/EditArticlePage.spec.tsx
+++ src/presentation/pages/EditArticlePage.spec.tsx
@@ -57,7 +57,7 @@ describe('EditArticlePage', () => {
   it('should render the edit article form when data is loaded', async () => {
     makeSut()
 
-    await screen.findByRole('form')
+    await screen.findByRole('form-edit-article')
   })
 
   it('should render the form header', async () => {
