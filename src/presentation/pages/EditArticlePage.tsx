6037f43615f143d3eacef8166994dc2e92f803a3
decante <gustavocamposantunes@gmail.com>
2025-07-10 16:23:57 -0300

    feat: add PageTemplate in EditArticlePage

--- src/presentation/pages/EditArticlePage.tsx
+++ src/presentation/pages/EditArticlePage.tsx
@@ -11,6 +11,7 @@ import { Label } from '../components/ui/label'
 import { Textarea } from '../components/ui/textarea'
 import { Input } from '../components/ui/input'
 import { toBase64 } from '../utils/toBase64'
+import { PageTemplate } from '../components/templates'
 
 type EditArticlePageProps = {
   getArticletById: GetArticleByIdUseCase
@@ -44,67 +45,72 @@ export const EditArticlePage: React.FC<EditArticlePageProps> = ({
       })
   }, [data?.data])
 
-  let content = isLoading ? <span data-testid="skeleton-group">
-    <Skeleton />
-    <Skeleton />
-    <Skeleton />
-  </span> : <form role='form'>
-    <FormHeader title='Editar Artigo' />
-    <section className="mt-4 flex flex-col gap-4">
-      <div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="picture">Inserir Imagem</Label>
-          <Input
-            id="picture"
-            type="file"
-            data-testid="input-picture"
-            onChange={async (e) => {
-              const file = e.target.files?.[0]
-              if (file) {
-                const base64 = await toBase64(file)
-                setEditArticleParams({
-                  ...editArticleParams,
-                  image: base64,
-                })
-              }
-            }}
-          />
-        </div>
-        {editArticleParams.image && (
-          <img
-            className="w-72"
-            src={editArticleParams.image}
-            alt="foto do artigo selecionada"
-            data-testid="selected-image"
-          />
-        )}
-      </div>
-      <div className="grid w-full gap-1.5">
-        <Label htmlFor="title">Título</Label>
-        <Textarea
-          placeholder="Edite o título"
-          id="title"
-          name='title'
-          data-testid="textaread-title"
-          onChange={(e) => setEditArticleParams({...editArticleParams, title: e.target.value})}
-          value={editArticleParams.title}
-        />
-      </div>
+  let content = isLoading ? 
+    <span data-testid="skeleton-group">
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+    </span> 
+    : 
+    <PageTemplate>
+      <form role='form'>
+        <FormHeader title='Editar Artigo' />
+        <section className="mt-4 flex flex-col gap-4">
+          <div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="picture">Inserir Imagem</Label>
+              <Input
+                id="picture"
+                type="file"
+                data-testid="input-picture"
+                onChange={async (e) => {
+                  const file = e.target.files?.[0]
+                  if (file) {
+                    const base64 = await toBase64(file)
+                    setEditArticleParams({
+                      ...editArticleParams,
+                      image: base64,
+                    })
+                  }
+                }}
+              />
+            </div>
+            {editArticleParams.image && (
+              <img
+                className="w-72"
+                src={editArticleParams.image}
+                alt="foto do artigo selecionada"
+                data-testid="selected-image"
+              />
+            )}
+          </div>
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="title">Título</Label>
+            <Textarea
+              placeholder="Edite o título"
+              id="title"
+              name='title'
+              data-testid="textaread-title"
+              onChange={(e) => setEditArticleParams({...editArticleParams, title: e.target.value})}
+              value={editArticleParams.title}
+            />
+          </div>
 
-      <div className="grid w-full gap-1.5">
-        <Label htmlFor="content">Conteúdo</Label>
-        <Textarea
-          className="min-h-[400px]"
-          placeholder="Edite o conteúdo"
-          id="content"
-          name='content'
-          data-testid="textaread-content"
-          onChange={(e) => setEditArticleParams({...editArticleParams, content: e.target.value})}
-          value={editArticleParams.content}
-        />
-      </div>
-    </section>
-  </form>
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="content">Conteúdo</Label>
+            <Textarea
+              className="min-h-[400px]"
+              placeholder="Edite o conteúdo"
+              id="content"
+              name='content'
+              data-testid="textaread-content"
+              onChange={(e) => setEditArticleParams({...editArticleParams, content: e.target.value})}
+              value={editArticleParams.content}
+            />
+          </div>
+        </section>
+      </form>
+    </PageTemplate>
 
   return content
 }
