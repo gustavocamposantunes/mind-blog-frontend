2379c35f8e8aeb67ae44a5add821da0d7873ee00
decante <gustavocamposantunes@gmail.com>
2025-07-11 21:15:58 -0300

    docs(storybook): create MenuItem storybook

---
+++ src/stories/atoms/MenuItem.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { MenuItem } from '@/presentation/components/atoms'
+import { Newspaper } from 'lucide-react'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+
+const meta = {
+  title: 'Atoms/MenuItem',
+  component: MenuItem,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    redirect: "/",
+    children: "Artigos",
+    iconStart: <Newspaper />
+  },
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Story />} />
+        </Routes>
+      </MemoryRouter>
+    ),
+  ],
+} satisfies Meta<typeof MenuItem>
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
