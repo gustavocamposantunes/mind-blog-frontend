3a79ea094779726bd4c7dac721c9686c4ed2c2d5
decante <gustavocamposantunes@gmail.com>
2025-07-11 21:39:11 -0300

    docs(storybook): create CustomSkeleton storybook

---
+++ src/stories/atoms/CustomSkeleton.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { CustomSkeleton } from '@/presentation/components/atoms'
+
+const meta = {
+  title: 'Atoms/CustomSkeleton',
+  component: CustomSkeleton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [(Story) => <section className="w-4xl">{Story()}</section>],
+} satisfies Meta<typeof CustomSkeleton>
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
