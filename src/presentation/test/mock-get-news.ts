22001c211cbf791abba05388ac94203f5b8e0eb3
decante <gustavocamposantunes@gmail.com>
2025-07-13 18:38:29 -0300

    refactor: remove timeout from GetNewsSpy

--- src/presentation/test/mock-get-news.ts
+++ src/presentation/test/mock-get-news.ts
@@ -5,16 +5,10 @@ import type { GetNewsUseCase } from '@/domain/usecases/news/get-news.usecase'
 import { mockNews } from '@/domain/test'
 
 export class GetNewsSpy implements GetNewsUseCase {
-  private readonly timeout: number
   news = mockNews()
   promise = undefined
 
-  constructor(timeout = 0) {
-    this.timeout = timeout
-  }
-
   getNews(): Promise<HttpRemoteResponse<NewsModel>> {
-    setTimeout(() => {}, this.timeout)
     return Promise[this.promise ?? 'resolve']({
       statusCode: 200,
       data: this.news,
