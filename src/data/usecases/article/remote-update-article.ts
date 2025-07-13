d08aa8d9be1c59b4776f82afc4d99bcafd03f5cb
decante <gustavocamposantunes@gmail.com>
2025-07-13 01:26:40 -0300

    feat: ensure RemoteUpdateArticle returns an InternalServerError if HttpPutClient returns 500

--- src/data/usecases/article/remote-update-article.ts
+++ src/data/usecases/article/remote-update-article.ts
@@ -1,4 +1,5 @@
-import type { HttpPutClient } from '@/data/protocols'
+import { HttpStatusCode, type HttpPutClient } from '@/data/protocols'
+import { InternalServerError } from '@/domain/errors'
 
 export class RemoteUpdateArticle {
   private readonly url: string
@@ -13,12 +14,20 @@ export class RemoteUpdateArticle {
     token: string,
     updateArticleParams: { title?: string; content?: string; image?: string },
   ) {
-    await this.httpClient.put({
+    const { status } = await this.httpClient.put({
       url: this.url,
       body: updateArticleParams,
       headers: {
         ...(token && { Authorization: `Bearer ${token}` }),
       },
     })
+
+    switch (status) {
+      case HttpStatusCode.serverError:
+        return {
+          statusCode: status,
+          error: new InternalServerError().message,
+        }
+    }
   }
 }
