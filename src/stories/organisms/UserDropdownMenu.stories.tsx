bd71f6cedc668c64996c3e539538438b5e04ba86
decante <gustavocamposantunes@gmail.com>
2025-07-13 22:29:43 -0300

    docs(storybook): create UserDropdownMenu organism storybook

---
+++ src/stories/organisms/UserDropdownMenu.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { mockAuthenticateUserModel } from '@/domain/test'
+import { UserDropdownMenu } from '@/presentation/components/organism'
+
+const meta = {
+  title: 'Organisms/UserDropdownMenu',
+  component: UserDropdownMenu,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    user: mockAuthenticateUserModel().user,
+    onLogout: () => {},
+    onProfileNavigate: () => {},
+  },
+} satisfies Meta<typeof UserDropdownMenu>
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
