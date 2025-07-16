4c9f65314c6aeee34198338d4b9140f151808709
decante <gustavocamposantunes@gmail.com>
2025-07-16 11:12:06 -0300

    test: ensure HomePage redirects to articles with correct limit and page

--- src/presentation/pages/HomePage.spec.tsx
+++ src/presentation/pages/HomePage.spec.tsx
@@ -1,6 +1,4 @@
-import { beforeEach } from 'node:test'
-
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, vi, beforeEach } from 'vitest'
 
 import { GetNewsSpy, renderHomePageWithRouter } from '../test'
 
@@ -10,6 +8,16 @@ vi.mock('react-router-dom', async () => ({
   ...(await vi.importActual('react-router-dom')),
   useNavigate: () => vi.fn(),
 }))
+
+vi.mock('react-responsive', async () => {
+  return {
+    useMediaQuery: vi.fn(),
+  }
+})
+
+const useMediaQueryMock = vi.mocked(
+  await import('react-responsive').then((mod) => mod.useMediaQuery),
+)
 
 type SutTypes = {
   getNewsSpy: GetNewsSpy
@@ -29,6 +37,32 @@ describe('HomePage', () => {
     vi.clearAllMocks()
     cleanup()
   })
+  describe('Header', () => {
+    it.each([
+      [1024, 5, 'mobile'],
+      [1279, 10, 'large'],
+      [0, 12, 'extra large'],
+    ])(
+      'should render /articles with page=1 and limit=%i when screen is %s',
+      (mockWidth, expectedLimit) => {
+        useMediaQueryMock.mockImplementation(({ maxWidth }) => {
+          return maxWidth === mockWidth
+        })
+
+        makeSut()
+
+        const articlesLink = screen.getByRole('link', {
+          name: /artigos/i,
+        })
+
+        expect(articlesLink).toHaveAttribute(
+          'href',
+          `/articles?page=1&limit=${expectedLimit}`,
+        )
+      },
+    )
+  })
+
   describe('Featured Articles', () => {
     it('should render a skeleton while promise is pending', () => {
       makeSut()
