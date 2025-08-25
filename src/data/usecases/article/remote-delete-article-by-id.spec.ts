0ea09fb543640daebaf12c357363508b75016269
decante <gustavocamposantunes@gmail.com>
2025-08-25 13:42:28 -0300

    refactor: create an SUT function for RemoteDeleteArticleById

--- src/data/usecases/article/remote-delete-article-by-id.spec.ts
+++ src/data/usecases/article/remote-delete-article-by-id.spec.ts
@@ -5,11 +5,25 @@ import { RemoteDeleteArticleById } from './remote-delete-article-by-id'
 
 import { HttpDeleteClientSpy } from '@/data/test/mock-http-client'
 
+type SutTypes = {
+  sut: RemoteDeleteArticleById
+  httpDeleteClientSpy: HttpDeleteClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpDeleteClientSpy = new HttpDeleteClientSpy()
+  const sut = new RemoteDeleteArticleById(url, httpDeleteClientSpy)
+
+  return {
+    sut,
+    httpDeleteClientSpy,
+  }
+}
+
 describe('RemoteDeleteArticleById', () => {
   it('should call HttpDeleteClient with correct URL', async () => {
     const url = faker.internet.url()
-    const httpDeleteClientSpy = new HttpDeleteClientSpy()
-    const sut = new RemoteDeleteArticleById(url, httpDeleteClientSpy)
+    const { sut, httpDeleteClientSpy } = makeSut(url)
     const articleId = faker.string.uuid()
 
     await sut.deleteById(articleId)
