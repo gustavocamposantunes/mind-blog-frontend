e24f7130f70a814f80fc8250a18cd65a66bd72c3
decante <gustavocamposantunes@gmail.com>
2025-08-26 09:05:23 -0300

    feat: ensure CustomCard render card header with correct title

--- src/presentation/components/organism/CustomCard.tsx
+++ src/presentation/components/organism/CustomCard.tsx
@@ -1,15 +1,20 @@
-import { Card } from '../ui/card'
+import { Card, CardHeader, CardTitle } from '../ui/card'
 
 interface ICustomCard {
   id: string
   onClick(): void
+  title: string
 }
 
-export const CustomCard: React.FC<ICustomCard> = ({ id, onClick }) => (
+export const CustomCard: React.FC<ICustomCard> = ({ id, onClick, title }) => (
   <Card
     className="pt-0 cursor-pointer hover:shadow-lg transition-shadow"
     key={id}
     onClick={onClick}
     data-testid={`custom-card-${id}`}
-  ></Card>
+  >
+    <CardHeader data-testid="custom-card-header">
+      <CardTitle data-testid="header-title">{title}</CardTitle>
+    </CardHeader>
+  </Card>
 )
