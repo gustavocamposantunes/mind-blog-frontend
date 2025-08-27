93680a3a1239f672b2c4f96d01f817f36e6b2dda
decante <gustavocamposantunes@gmail.com>
2025-08-27 08:11:46 -0300

    feat: ensure FavoriteButton init the icon fill red when isFavoritedIs passed as true

--- src/presentation/components/atoms/FavoriteButton.tsx
+++ src/presentation/components/atoms/FavoriteButton.tsx
@@ -3,8 +3,14 @@ import { useState } from 'react'
 
 import { Button } from '../ui/button'
 
-export const FavoriteButton = () => {
-  const [toogleFavorite, setToogleFavorite] = useState(false)
+interface IFavoriteButton {
+  isFavorited?: boolean
+}
+
+export const FavoriteButton: React.FC<IFavoriteButton> = ({
+  isFavorited = false,
+}) => {
+  const [toogleFavorite, setToogleFavorite] = useState(isFavorited)
   return (
     <Button data-testid="favorite-btn" onClick={() => setToogleFavorite(true)}>
       <Heart
