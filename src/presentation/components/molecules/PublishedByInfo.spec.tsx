60f874511784fc76f0d9aacf840ba39a5a95e542
decante <gustavocamposantunes@gmail.com>
2025-08-28 09:00:50 -0300

    test: ensure PublishedByInfo render the published date

--- src/presentation/components/molecules/PublishedByInfo.spec.tsx
+++ src/presentation/components/molecules/PublishedByInfo.spec.tsx
@@ -3,20 +3,34 @@ import { describe, it, expect, beforeEach } from 'vitest'
 
 import { PublishedByInfo } from './PublishedByInfo'
 
-import { render, screen } from '@/presentation/test/test-utils'
+import { cleanup, render, screen } from '@/presentation/test/test-utils'
+import { formatDateToShortMonth } from '@/presentation/utils/dateFormatter'
 
-const makeSut = () => {
+const makeSut = (publishedAt = faker.date.anytime().toISOString()) => {
   const avatar = faker.image.avatar()
-  render(<PublishedByInfo avatar={avatar} />)
+  render(<PublishedByInfo avatar={avatar} publishedAt={publishedAt} />)
 }
 
 describe('PublishedByInfo', () => {
-  beforeEach(() => {
-    makeSut()
-  })
+  beforeEach(cleanup)
   it('should render the avatar correctly', () => {
+    makeSut()
+
     const customAvatar = screen.getByTestId('avatar')
 
     expect(customAvatar).toBeInTheDocument()
+  })
+
+  it('should render the published at date', () => {
+    const publishedAt = faker.date.anytime().toISOString()
+
+    makeSut(publishedAt)
+
+    const publishedByInfo = screen.getByTestId('published-by-info')
+
+    expect(publishedByInfo).toBeInTheDocument()
+    expect(publishedByInfo.textContent).toEqual(
+      expect.stringContaining(formatDateToShortMonth(publishedAt)),
+    )
   })
 })
