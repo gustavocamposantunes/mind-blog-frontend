7f85b0447eba6dc3b8e04e1764aaf77f68901ccf
decante <gustavocamposantunes@gmail.com>
2025-08-28 09:01:04 -0300

    feat: ensure PublishedByInfo render the published date

--- src/presentation/components/molecules/PublishedByInfo.tsx
+++ src/presentation/components/molecules/PublishedByInfo.tsx
@@ -3,14 +3,29 @@ import {
   AvatarImage,
   AvatarFallback,
 } from '@/presentation/components/ui/avatar'
+import { formatDateToShortMonth } from '@/presentation/utils/dateFormatter'
 
 interface IPublishedByInfo {
   avatar: string
+  publishedAt: string
 }
 
-export const PublishedByInfo: React.FC<IPublishedByInfo> = ({ avatar }) => (
-  <Avatar data-testid="avatar">
-    <AvatarImage data-testid="avatar-image" src={avatar} alt="avatar image" />
-    <AvatarFallback>CN</AvatarFallback>
-  </Avatar>
-)
+export const PublishedByInfo: React.FC<IPublishedByInfo> = ({
+  avatar,
+  publishedAt,
+}) => {
+  const formattedDate = formatDateToShortMonth(publishedAt)
+  return (
+    <div>
+      <Avatar data-testid="avatar">
+        <AvatarImage
+          data-testid="avatar-image"
+          src={avatar}
+          alt="avatar image"
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <span data-testid="published-by-info">{formattedDate}</span>
+    </div>
+  )
+}
