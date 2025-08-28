ec904989463d1ee11b91893a789cbe0b29ac7b90
decante <gustavocamposantunes@gmail.com>
2025-08-28 08:23:33 -0300

    refactor: move PublishedByInfo render to a SUT function

--- src/presentation/components/molecules/PublishedByInfo.spec.tsx
+++ src/presentation/components/molecules/PublishedByInfo.spec.tsx
@@ -1,15 +1,20 @@
 import { faker } from '@faker-js/faker'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { PublishedByInfo } from './PublishedByInfo'
 
 import { render, screen } from '@/presentation/test/test-utils'
 
-describe('PublishedByInfo', () => {
-  it('should render the avatar correctly', () => {
-    const avatar = faker.image.avatar()
-    render(<PublishedByInfo avatar={avatar} />)
+const makeSut = () => {
+  const avatar = faker.image.avatar()
+  render(<PublishedByInfo avatar={avatar} />)
+}
 
+describe('PublishedByInfo', () => {
+  beforeEach(() => {
+    makeSut()
+  })
+  it('should render the avatar correctly', () => {
     const customAvatar = screen.getByTestId('avatar')
 
     expect(customAvatar).toBeInTheDocument()
