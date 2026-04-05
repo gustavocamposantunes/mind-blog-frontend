be4bb3caa62b82502476468539f15079c94b4e2b
decante <gustavocamposantunes@gmail.com>
2026-04-05 10:32:43 -0300

    fix(commitlint): set header-max-length rule to enforce 120 character limit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,9 +2,10 @@ import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
-  rules: {},
+  rules: {
+    'header-max-length': [2, 'always', 120],
+  },
 }
 
 export default Configuration
