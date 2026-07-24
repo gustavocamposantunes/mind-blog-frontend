dd30560826303d3abb64dbd307c0d27ffbae6821
decante <gustavocamposantunes@gmail.com>
2026-07-23 22:10:53 -0300

    chore: remove unused news-related files and components

--- src/domain/usecases/news/get-news.usecase.ts
+++
@@ -1,6 +0,0 @@
-import type { HttpRemoteResponse } from '@/data/protocols'
-import type { NewsModel } from '@/domain/models'
-
-export interface GetNewsUseCase {
-  getNews(): Promise<HttpRemoteResponse<NewsModel>>
-}
